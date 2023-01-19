a352cc37a9d3f1d7d09553325423167639d4a851
Joe Wolf <joe.wolf@quest.com>
2023-01-19 12:01:30 -0800

    removing old sdk

--- examples/onelogin_role_example.tf
+++
@@ -1,6 +0,0 @@
-resource onelogin_roles executive_admin {
-  name = "executive admin"
-  apps = []
-  users = []
-  admins= []
-}
--- examples/onelogin_user_updated_example.tf
+++
@@ -1,4 +0,0 @@
-resource onelogin_users basic_test {
-  username = "boaty.mcboatface"
-  email = "boaty.mcboatface@onelogin.com"
-}
