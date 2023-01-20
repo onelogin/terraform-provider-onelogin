062ffafa7078f471b06ba8051ce68646a46e562b
Joe Wolf <joe.wolf@quest.com>
2023-01-20 11:37:32 -0800

    adding go packages

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"github.com/dikhan/terraform-provider-openapi/v3/openapi"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+	"log"
+)
+
+	// Version specifies the version of the provider (will be set statically at compile time)
+	Version = "dev"
+	// Commit specifies the commit hash of the provider at the time of building the binary (will be set statically at compile time)
+	Commit = "none"
+	// Date specifies the data which the binary was build (will be set statically at compile time)
+	Date = "unknown"
+
+func main() {
+
+	log.Printf("[INFO] Running Terraform Provider %s v%s-%s; Released on: %s", ProviderName, Version, Commit, Date)
+
+	log.Printf("[INFO] Initializing OpenAPI Terraform provider '%s' with service provider's OpenAPI document: %s", ProviderName, ProviderOpenAPIURL)
+
+	providerName = "onelogin"
+	providerOpenAPIURL = "https://raw.githubusercontent.com/onelogin/terraform-provider-onelogin/openapi/swag-api.yml"
+
+	p := openapi.ProviderOpenAPI{ProviderName: providerName}
+	serviceProviderConfig := &openapi.ServiceConfigV1{
+		SwaggerURL: providerOpenAPIURL,
+	}
+
+	provider, err := p.CreateSchemaProviderFromServiceConfiguration(serviceProviderConfig)
+	if err != nil {
+		log.Fatalf("[ERROR] Failed to initialize the terraform provider: %s", err)
+	}
+
+	plugin.Serve(
+		&plugin.ServeOpts{
+			ProviderFunc: func() *schema.Provider {
+				return provider
+			},
+		},
+	)
+}
