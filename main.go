beb3360ef3edc6ff2165c246d3f58541e8cee00e
Joe Wolf <joe.wolf@quest.com>
2023-01-26 14:17:43 -0800

    fixed middle

--- main.go
+++ main.go
@@ -8,23 +8,26 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
 
-// Version specifies the version of the provider (will be set statically at compile time)
- Version = "dev"
-// Commit specifies the commit hash of the provider at the time of building the binary (will be set statically at compile time)
- Commit = "none"
-// Date specifies the data which the binary was build (will be set statically at compile time)
- Date = "unknown"
+var (
+	// Version specifies the version of the provider (will be set statically at compile time)
+	Version = "dev"
+	// Commit specifies the commit hash of the provider at the time of building the binary (will be set statically at compile time)
+	Commit = "none"
+	// Date specifies the data which the binary was build (will be set statically at compile time)
+	Date = "unknown"
+
 // Generate the Terraform provider documentation using `tfplugindocs`:
-//
+)
+
 //go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
 func main() {
 
-	log.Printf("[INFO] Running Terraform Provider %s v%s-%s; Released on: %s", ProviderName, Version, Commit, Date)
+	//log.Printf("[INFO] Running Terraform Provider %s v%s-%s; Released on: %s", ProviderName, Version, Commit, Date)
 
-	log.Printf("[INFO] Initializing OpenAPI Terraform provider '%s' with service provider's OpenAPI document: %s", ProviderName, ProviderOpenAPIURL)
+	//log.Printf("[INFO] Initializing OpenAPI Terraform provider '%s' with service provider's OpenAPI document: %s", ProviderName, ProviderOpenAPIURL)
 
-	providerName = "onelogin"
-	providerOpenAPIURL = "https://raw.githubusercontent.com/onelogin/terraform-provider-onelogin/openapi/swag-api.yml"
+	var providerName = "onelogin"
+	var providerOpenAPIURL = "https://raw.githubusercontent.com/onelogin/terraform-provider-onelogin/openapi/swag-api.yml"
 
 	p := openapi.ProviderOpenAPI{ProviderName: providerName}
 	serviceProviderConfig := &openapi.ServiceConfigV1{
