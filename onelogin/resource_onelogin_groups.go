60d53fe0d69c61461d6884e686f77af3b1ac76ee
Will Munslow <will@subterrane.com>
2026-03-13 15:16:51 -0700

    feat: implement full CRUD for onelogin_groups via V2 API
    - Wire up groupCreate, groupUpdate, groupDelete using SDK v4.8.0
    - Use V2 API (GetGroupByIDV2WithContext) for reads
    - Use d.GetOk for optional reference field to avoid sending empty strings
    - Check d.Set errors in groupRead
    - Add CheckDestroy and ImportState test steps
    - Update documentation to reflect full CRUD support
    
    Fixes #204

--- onelogin/resource_onelogin_groups.go
+++ onelogin/resource_onelogin_groups.go
@@ -2,6 +2,7 @@ package onelogin
 
 import (
 	"context"
+	"fmt"
 	"strconv"
 
 	"github.com/hashicorp/terraform-plugin-log/tflog"
@@ -42,50 +43,46 @@ func resourceOneLoginGroups() *schema.Resource {
 
 // groupCreate creates a new OneLogin Group
 func groupCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	_, err := groupschema.Inflate(map[string]interface{}{
-		"name":      d.Get("name"),
-		"reference": d.Get("reference"),
-	})
+	inflateMap := map[string]interface{}{
+		"name": d.Get("name"),
+	}
+	if ref, ok := d.GetOk("reference"); ok {
+		inflateMap["reference"] = ref
+	}
+
+	group, err := groupschema.Inflate(inflateMap)
 	if err != nil {
 		return utils.HandleSchemaError(ctx, err, utils.ErrorCategoryCreate, "Group", "")
 	}
 
-	// Note: The OneLogin SDK doesn't currently have a CreateGroup method
-	// This is a placeholder for when that functionality is added
-	// For now, we'll return an error indicating this isn't implemented yet
-	return diag.Errorf("Creating groups is not yet supported by the OneLogin API")
+	client := m.(*onelogin.OneloginSDK)
+	tflog.Info(ctx, "[CREATE] Creating group", map[string]interface{}{
+		"name": group.Name,
+	})
 
-	// Once the SDK supports group creation, the code would look something like:
-	/*
-		client := m.(*onelogin.OneloginSDK)
-		tflog.Info(ctx, "[CREATE] Creating group", map[string]interface{}{
-			"name": group.Name,
-		})
+	result, err := client.CreateGroupWithContext(ctx, &group)
+	if err != nil {
+		return utils.HandleAPIError(ctx, err, utils.ErrorCategoryCreate, "Group", "")
+	}
 
-		result, err := client.CreateGroup(group)
-		if err != nil {
-			return utils.HandleAPIError(ctx, err, utils.ErrorCategoryCreate, "Group", "")
-		}
+	groupMap, ok := result.(map[string]interface{})
+	if !ok {
+		return diag.Errorf("failed to parse group creation response")
+	}
 
-		// Extract group ID from the result
-		groupMap, ok := result.(map[string]interface{})
-		if !ok {
-			return diag.Errorf("failed to parse group creation response")
-		}
+	id, ok := groupMap["id"].(float64)
+	if !ok {
+		return diag.Errorf("failed to extract group ID from response")
+	}
 
-		id, ok := groupMap["id"].(float64)
-		if !ok {
-			return diag.Errorf("failed to extract group ID from response")
-		}
+	groupID := int(id)
+	tflog.Info(ctx, "[CREATED] Created group", map[string]interface{}{
+		"id":   groupID,
+		"name": group.Name,
+	})
 
-		groupID := int(id)
-		tflog.Info(ctx, "[CREATED] Created group", map[string]interface{}{
-			"id": groupID,
-		})
-
-		d.SetId(fmt.Sprintf("%d", groupID))
-		return groupRead(ctx, d, m)
-	*/
+	d.SetId(fmt.Sprintf("%d", groupID))
+	return groupRead(ctx, d, m)
 }
 
 // groupRead reads a OneLogin Group by ID
@@ -100,12 +97,11 @@ func groupRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.
 		"id": groupID,
 	})
 
-	result, err := client.GetGroupByID(groupID)
+	result, err := client.GetGroupByIDV2WithContext(ctx, groupID)
 	if err != nil {
 		return utils.HandleAPIError(ctx, err, utils.ErrorCategoryRead, "Group", d.Id())
 	}
 
-	// Check if group exists
 	if result == nil {
 		tflog.Info(ctx, "[NOT FOUND] Group not found", map[string]interface{}{
 			"id": groupID,
@@ -114,19 +110,16 @@ func groupRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.
 		return nil
 	}
 
-	// Parse the group map from the result
 	groupMap, ok := result.(map[string]interface{})
 	if !ok {
 		return diag.Errorf("failed to parse group response")
 	}
 
-	// Set basic fields
-	if name, ok := groupMap["name"].(string); ok {
-		d.Set("name", name)
+	if err := d.Set("name", groupMap["name"]); err != nil {
+		return diag.FromErr(err)
 	}
-
-	if ref, ok := groupMap["reference"].(string); ok {
-		d.Set("reference", ref)
+	if err := d.Set("reference", groupMap["reference"]); err != nil {
+		return diag.FromErr(err)
 	}
 
 	return nil
@@ -139,51 +132,44 @@ func groupUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) dia
 		return diag.FromErr(err)
 	}
 
-	_, err = groupschema.Inflate(map[string]interface{}{
-		"id":        groupID,
-		"name":      d.Get("name"),
-		"reference": d.Get("reference"),
-	})
+	inflateMap := map[string]interface{}{
+		"id":   groupID,
+		"name": d.Get("name"),
+	}
+	if ref, ok := d.GetOk("reference"); ok {
+		inflateMap["reference"] = ref
+	}
+
+	group, err := groupschema.Inflate(inflateMap)
 	if err != nil {
 		return utils.HandleSchemaError(ctx, err, utils.ErrorCategoryUpdate, "Group", d.Id())
 	}
 
-	// Note: The OneLogin SDK doesn't currently have an UpdateGroup method
-	// This is a placeholder for when that functionality is added
-	return diag.Errorf("Updating groups is not yet supported by the OneLogin API")
+	client := m.(*onelogin.OneloginSDK)
+	tflog.Info(ctx, "[UPDATE] Updating group", map[string]interface{}{
+		"id": groupID,
+	})
 
-	// Once the SDK supports group updates, the code would look something like:
-	/*
-		client := m.(*onelogin.OneloginSDK)
-		tflog.Info(ctx, "[UPDATE] Updating group", map[string]interface{}{
-			"id": groupID,
-		})
+	_, err = client.UpdateGroupWithContext(ctx, groupID, &group)
+	if err != nil {
+		return utils.HandleAPIError(ctx, err, utils.ErrorCategoryUpdate, "Group", d.Id())
+	}
 
-		_, err = client.UpdateGroup(groupID, group)
-		if err != nil {
-			return utils.HandleAPIError(ctx, err, utils.ErrorCategoryUpdate, "Group", d.Id())
-		}
+	tflog.Info(ctx, "[UPDATED] Updated group", map[string]interface{}{
+		"id": groupID,
+	})
 
-		tflog.Info(ctx, "[UPDATED] Updated group", map[string]interface{}{
-			"id": groupID,
-		})
-
-		return groupRead(ctx, d, m)
-	*/
+	return groupRead(ctx, d, m)
 }
 
 // groupDelete deletes a OneLogin Group
 func groupDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	// Note: The OneLogin SDK doesn't currently have a DeleteGroup method
-	// This is a placeholder for when that functionality is added
-	return diag.Errorf("Deleting groups is not yet supported by the OneLogin API")
-
-	// Once the SDK supports group deletion, the code would look something like:
-	/*
-		client := m.(*onelogin.OneloginSDK)
-		return utils.StandardDeleteFunc(ctx, d, func(id string) (interface{}, error) {
-			aid, _ := strconv.Atoi(id)
-			return client.DeleteGroup(aid)
-		}, "Group")
-	*/
+	client := m.(*onelogin.OneloginSDK)
+	return utils.StandardDeleteFunc(ctx, d, func(id string) (interface{}, error) {
+		aid, err := strconv.Atoi(id)
+		if err != nil {
+			return nil, err
+		}
+		return client.DeleteGroupWithContext(ctx, aid)
+	}, "Group")
 }
